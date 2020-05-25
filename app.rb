c2f06b359b9ca6e2e506f71d4be819deb7104636
Arina Zaitseva <avanero@bk.ru>
2020-05-25 13:03:27 +0700

    use Rack::Response

--- app.rb
+++ app.rb
@@ -2,28 +2,42 @@ require_relative 'time_handler'
 
 class App
   def call(env)
-    request = Rack::Request.new(env)
-    path = request.path
-    time = TimeHandler.new(request.params)
-    
-    if path != '/time'
-      return response(404, "Page not found")
-    end
-
-    if time.unknown.empty?
-      response(200, Time.now.strftime(time.valid))
-    else
-      response(400, "Unknown time format #{time.unknown}")
-    end
+    @request = Rack::Request.new(env)
+    @response = Rack::Response.new
+    handle_time_request
   end
-  
+
   private
-  
-  def response(status, body)
-    [
-      status,
-      {'Content-Type' => 'text/plain'},
-      [body]
-    ]
+
+  def handle_time_request
+    @response['Content-Type'] = 'text/plain'
+    return not_found_response if @request.path != '/time'
+    return no_format_response if @request.params['format'].nil?
+
+    @time = TimeHandler.new(@request.params)
+    @time.call(@request.params['format'].split(','))
+    perform_format_response
+  end
+
+  def not_found_response
+    @response.write 'Page not found'
+    @response.status = 404
+    @response.finish
+  end
+
+  def no_format_response
+    @response.write 'No time format mentioned'
+    @response.status = 400
+    @response.finish
+  end
+
+  def perform_format_response
+    if @time.valid?
+      @response.write @time.formatted.to_s
+    else
+      @response.write "Unknown time format #{@time.unknown_format}"
+      @response.status = 400
+    end
+    @response.finish
   end
 end
